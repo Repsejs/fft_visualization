8ad0f2f35245e0c9aa2f695fe81c34a96d68e6ac
Repsejs <repsejs@hotmail.com>
2022-09-26 17:22:53 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,62 @@
+#include "gd32vf103.h"
+#include "drivers.h"
+#include "eclicw.h"
+#include "lcd.h"
+#include "delay.h"
+#include "adc.h"
+#include "fft.h"
+#include "cordic-math.h"
+#define EI 1
+#define DI 0
+#define SCREENHEIGHT 80
+#define WAVEPOINTS 512
+
+Complex wave[WAVEPOINTS]={0};
+
+int main(void)
+{ 
+  for (int i = 0; i < WAVEPOINTS; i++)
+  {
+    wave[i].real = 0;
+    wave[i].imag = 0;
+  }
+  
+  int adcr;
+  int value;
+  int amount = 0;
+  int length;
+  ADC3powerUpInit(0); // Initialize ADC0, Ch3 & Ch16
+  Lcd_SetType(LCD_INVERTED);
+  Lcd_Init();
+  LCD_Clear(BLACK);
+
+  while (1)
+  {
+    //uint64_t time = get_timer_value();
+    if (adc_flag_get(ADC0, ADC_FLAG_EOC) == SET)
+    { // ...ADC done?
+      value = adc_regular_data_read(ADC0);
+      wave[amount].real = ((value-2047)>>4);
+      wave[amount++].imag = 0;
+      adc_flag_clear(ADC0, ADC_FLAG_EOC); // ......clear IF
+      //delay_1us(10);
+      adc_software_trigger_enable(ADC0, // Trigger another ADC conversion!
+                                ADC_REGULAR_CHANNEL);
+    }
+    if(amount==WAVEPOINTS){
+      delay_1ms(100);
+      LCD_Clear(BLACK);
+      LCD_Wait_On_Queue();
+      fft(wave,WAVEPOINTS);
+      draw_graph();
+      for (int i = 0; i < 160; i++)
+      {
+        LCD_DrawLine(i,40,i,(-cordic_hypotenuse(wave[i].imag, wave[i].real)>>CORDIC_MATH_FRACTION_BITS)+40,RED);
+      }
+      
+      LCD_Wait_On_Queue();
+      amount=0;
+    }
+    
+  }
+}
