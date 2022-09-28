1dd5c3d44e6d4b6ba887e0aea8a1a9007210aefc
Repsejs <repsejs@hotmail.com>
2022-09-28 08:25:12 +0200

    inverse fft + print

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 #define EI 1
 #define DI 0
 #define SCREENHEIGHT 80
-#define WAVEPOINTS 512
+#define WAVEPOINTS 128
 
 Complex wave[WAVEPOINTS]={0};
 
@@ -39,7 +39,7 @@ int main(void)
       wave[amount].real = ((value-2047)>>4);
       wave[amount++].imag = 0;
       adc_flag_clear(ADC0, ADC_FLAG_EOC); // ......clear IF
-      //delay_1us(10);
+      delay_1us(7812);   //ändra för skala, 7812 = 1Hz
       adc_software_trigger_enable(ADC0, // Trigger another ADC conversion!
                                 ADC_REGULAR_CHANNEL);
     }
@@ -49,9 +49,18 @@ int main(void)
       LCD_Wait_On_Queue();
       fft(wave,WAVEPOINTS);
       draw_graph();
-      for (int i = 0; i < 160; i++)
+      for (int i = 0; i < 128; i++)
       {
         LCD_DrawLine(i,40,i,(-cordic_hypotenuse(wave[i].imag, wave[i].real)>>CORDIC_MATH_FRACTION_BITS)+40,RED);
+      }
+      inverse_fft(wave,WAVEPOINTS);
+      for(int i = 0; i < 10; i++){
+        wave[i+45].real = 0;
+        wave[i+45].imag = 0;
+      }
+      for (int i = 0; i < 128; i++)
+      {
+        LCD_DrawPoint(i,(wave[i].real/5)+40,BLUE);
       }
       
       LCD_Wait_On_Queue();
